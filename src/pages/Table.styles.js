5f6df5e7ee1e02395346f38bc1989473fa01fafc
Austin <austin.dober@gmail.com>
2021-08-04 09:37:10 -0400

    adjusting big screen viewing

--- src/pages/Table.styles.js
+++ src/pages/Table.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div `
     position: absolute;
     background: black;
-    height: 100vh;
+    height: 100vh fit-content;
     width: 100%;
     
     
@@ -12,7 +12,7 @@ export const Wrapper = styled.div `
         margin-top: 100px;
         
     }
-
+    
 `;
 
 export const PlayingTable = styled.div ` 
@@ -23,7 +23,8 @@ export const PlayingTable = styled.div `
     border-radius: 25%;
     max-width: var(--maxwidth);
     min-height: 500px;
-    @media screen and (max-width: 768px){
+    @media screen and (max-width: 768px)
+    {
         padding-bottom: 60px;
     }
     @media screen and (min-width: 1200px){
